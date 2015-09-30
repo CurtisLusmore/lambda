40840e374fe2b80f437d10215eea28eb5aeb7313
Curtis Lusmore <curtis.lusmore@unifysolutions.net>
2015-09-30 20:33:45 +1000

    #286: Defined Form

---
+++ lambda.hs
@@ -0,0 +1,10 @@
+type Atom = String
+
+data Form = Atom Atom
+          | Lambda Atom Form
+          | Apply Form Form
+
+instance Show Form where
+    show (Atom a) = a
+    show (Lambda x y) = "\\" ++ x ++ " . " ++ show y
+    show (Apply f x) = "(" ++ show f ++ " " ++ show x ++ ")"
